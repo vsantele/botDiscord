7622c1ab4b3fa70796c8e4af1f6c3c7cd673ceff
Victor Santelé <Vic16@hotmail.be>
2019-12-10 19:21:51 +0100

    maj

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const request = require('request');
 const client = new Discord.Client();
 
-require('dotenv').load();
+require('dotenv').config();
 const token = process.env.TOKEN_DISCORD;
 var darksky = function(callback){
 	var  url = `https://api.darksky.net/forecast/${process.env.DARKSKY_TOKEN}/50.7459924,3.2171203?lang=fr&units=si&exclude=flags,hourly`;
@@ -68,37 +68,41 @@ client.on('ready', () => {
   });
   
   client.on('message', msg => {
+    if(msg.author.bot) return;
     if (msg.content.toLowerCase() === 'ping') {
       msg.reply('pong');
     }
-    if (msg.content.toLowerCase() === 'pong') {
+     else if (msg.content.toLowerCase() === 'pong') {
         msg.reply('ping');
     }
-    if (msg.content.toLowerCase() === 'meteo' || msg.content.toLowerCase() === 'météo' || msg.content.toLowerCase() === 'wheater') {
+     else if (msg.content.toLowerCase() === 'meteo' || msg.content.toLowerCase() === 'météo' || msg.content.toLowerCase() === 'wheater') {
         darksky(function(err, previsions){
             if(err) return msg.reply(err);
         
-            msg.reply(`Il fait actuellement ${previsions.currently.temperature}°C avec un temps ${previsions.currently.summary}. \n Le temps sera ${previsions.nextHour.summary} \n Et demain il y aura un maximum de ${previsions.nextDay.temperatureHigh}°C et un minimum de ${previsions.nextDay.temperatureLow}°C avec un temps ${previsions.nextDay.summary} \n À bientôt pour d'autres prévisions! (svp abusez pas j'ai 1000 résultats par jours <3 `);
+            msg.channel.send(`Il fait actuellement ${previsions.currently.temperature}°C avec un temps ${previsions.currently.summary}. \n Le temps sera ${previsions.nextHour.summary} \n Et demain il y aura un maximum de ${previsions.nextDay.temperatureHigh}°C et un minimum de ${previsions.nextDay.temperatureLow}°C avec un temps ${previsions.nextDay.summary} \n À bientôt pour d'autres prévisions! (svp abusez pas j'ai 1000 résultats par jours <3 `);
         });
     }
-    if (msg.content.toLowerCase() === 'chien') {
+    else if (msg.content.toLowerCase() === 'chien') {
         chien(function(err,image){
             if(err) return msg.reply(err);
 
             msg.reply("Voici un chien rien que pour vous 💕", { file: image})
         })
     }
-    if (msg.content.toLowerCase() === 'chat') {
+    else if (msg.content.toLowerCase() === 'chat') {
         msg.reply('Voici un chat rien que pour vous 😻 \n https://lorempixel.com/640/480/cats')
     }
-    if(msg.content.toLowerCase() === 'chuck') {
+    else if(msg.content.toLowerCase() === 'chuck') {
         chuck(function(err,fact){
             if(err) return msg.reply(err);
-            msg.reply(`${fact}`)
+            msg.channel.send(`${fact}`)
         })
     }
-    if(msg.content.toLowerCase() === "et ça fait") {
-        msg.reply("BIM BAM BOUM!")
+    else if(msg.content.toLowerCase() === "et ça fait") {
+        msg.channel.send("BIM BAM BOUM!")
+    }
+    else if (msg.author.username === "WolfVic" && msg.content.toLowerCase() === "debug") {
+        msg.channel.send("debug: ")
     }
     
   });
