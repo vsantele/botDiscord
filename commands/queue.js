a200807699715823e530b75ccb1f36c85fa2971d
root <root@DESKTOP-HP4U9C6.localdomain>
2020-04-11 22:44:18 +0200

    fix

--- commands/queue.js
+++ commands/queue.js
@@ -2,7 +2,8 @@
 module.exports = {
   name: 'queue',
   description: 'Donne les chansons dans la file d\'attente',
-  execute(message, args, audio) {
+  execute(message, args, options) {
+    const {audio} = options
     message.channel.send(audio.getQueue())
   }
 }
