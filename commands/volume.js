350847e6790311f60b7e2c39312cbefcf9eacaaf
root <root@DESKTOP-HP4U9C6.localdomain>
2020-04-11 14:07:22 +0200

    fix

--- commands/volume.js
+++ commands/volume.js
@@ -1,7 +1,8 @@
 module.exports = {
   name: 'volume',
   description: 'Modifie le volume entre 0 et 200%',
-  execute(message, args, audio) {
+  execute(message, args, options) {
+    const {audio} = options
     audio.volume(args)
   }
 }
