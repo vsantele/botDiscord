f6a71f899db8e2616b89a215e107895bd3ace50a
root <root@DESKTOP-HP4U9C6.localdomain>
2020-05-03 21:51:13 +0200

    fix def plusieurs mots

--- commands/def.js
+++ commands/def.js
@@ -7,7 +7,7 @@ module.exports = {
   usage: '<mot>',
   async execute(message, args) {
     try {
-      const res = await dict(args[0])
+      const res = await dict(args.join(" "))
       message.channel.send(res, {split: true})
     } catch (e) {
       console.error('e :', e);
