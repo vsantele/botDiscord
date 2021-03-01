d15b977c5ac144360de513cee914efdb052ce667
Victor Santelé <vic16@hotmail.be>
2021-03-01 09:19:58 +0100

    random lang

--- modules/detectLang.js
+++ modules/detectLang.js
@@ -2,6 +2,10 @@ const LanguageDetect = require('languagedetect');
 const lngDetector = new LanguageDetect();
 lngDetector.setLanguageType('iso2')
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 
 function detectLang(text) {
 
@@ -24,7 +28,10 @@ function detectLang(text) {
       lang = 'fr-fr'
 
   }
-  return 'es-es'
+  // return 'it-it'
+  const langs = ["en-us", "fr-fr", "nl-nl", "es-es", "it-it", "ru-ru", "pt-pt", "de-de", "ar-eg", "bg-bg", "zh-cn", "hr - hr", "cs-cz", "da-dk", "fi-fi", "fr-ca", "el-gr", "he-il", "hi-in", "id-id", "hu-hu", "ja-jp", "nb-no", "pl-pl", "ro-ro", "sk-sk", "sv-se", "th-th","tr-tr"]
+  return langs[getRandomInt(langs.length)]
+  
   // return lang;
 }
 
