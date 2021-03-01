050876f256dcd50c83fa8f6f637e842adeb66398
Victor Santelé <vic16@hotmail.be>
2021-03-01 09:20:04 +0100

    nettoyage

--- modules/genius.js
+++ modules/genius.js
@@ -6,28 +6,25 @@ const Genius = new genius.Client(process.env.GENIUS_TOKEN)
 const lyrics = async (track) => {
   let res = []
   if (track.length > 2) {
-      try {
-        console.log('track :', track);
-        const songs = await Genius.songs.search(track)
-        const song = songs[0]
-        const lyrics = await song.lyrics()
-        const infos = {
-          fullTitle: song.fullTitle,
-          title: song.title,
-          image: song.image,
-          url: song.url,
-          artist: song.artist,
-          album: song.album,
-          releasedAt: song.releasedAt
-        }
-        //.split('\n').filter(line => !line.startsWith('[')).join('\n')
-        res[0] = lyrics
-        res[1] = song.fullTitle
-        res[2] = infos
-        return res
-      } catch (error) {
-        throw new Error(error)
-      }
+    console.log('track :', track);
+    const songs = await Genius.songs.search(track)
+    const song = songs[0]
+    const lyrics = await song.lyrics()
+    const infos = {
+      fullTitle: song.fullTitle,
+      title: song.title,
+      image: song.image,
+      url: song.url,
+      artist: song.artist,
+      album: song.album,
+      releasedAt: song.releasedAt
+    }
+    //.split('\n').filter(line => !line.startsWith('[')).join('\n')
+    res[0] = lyrics
+    res[1] = song.fullTitle
+    res[2] = infos
+    return res
+
   }
 }
 
